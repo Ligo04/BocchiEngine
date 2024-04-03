53a0d0496ddf212c16c4c6f1b1360d7693c305a4
ligo <1569731402@qq.com>
2024-04-03 13:41:52 +0800

    Update Engine.hpp and .clang-format

--- Engine/Source/Runtime/Engine.hpp
+++ Engine/Source/Runtime/Engine.hpp
@@ -1,13 +1,21 @@
+#include "Luna/RHI/SwapChain.hpp"
+#include "Luna/Runtime/Path.hpp"
+#include "Luna/Runtime/Ref.hpp"
 #include <Luna/Window/Window.hpp>
-
 namespace Bocchi
 {
+    using namespace Luna;
     class BocchiEngine
     {
-            static const float kFpsAlpha;
+            static const float k_fps_alpha;
 
         public:
             BocchiEngine() {}
             ~BocchiEngine() {}
+
+        public:
+            Path                 m_project_path;
+            Ref<Window::IWindow> m_window;
+            Ref<RHI::ISwapChain> m_swap_chain;
     };
-} //namespace Bocchi
+} // namespace Bocchi
